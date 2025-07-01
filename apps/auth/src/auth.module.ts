791f056d148153476337e03703a43097c7998ac8
Sneha Pal <Sneha1289pal@gmail.com>
2025-07-01 22:35:28 +0530

    feat: enhance AuthModule with TypeORM integration and user entity updates

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,13 +1,28 @@
 import { Module } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 // import { AuthController } from './auth.controller';
 // import { AuthService } from './auth.service';
 import { User } from './entities/user.entity';
 import { UsersModule } from './users/users.module';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'sneha',
+      database: 'new',
+      entities: [User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+    UsersModule,
+  ],
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class AuthModule {}
--- apps/auth/src/entities/user.entity.ts
+++ apps/auth/src/entities/user.entity.ts
@@ -2,12 +2,21 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
   @Column()
-  username: string;
+  name: string;
+
+  @Column({ unique: true })
+  phoneNumber: string;
 
   @Column()
   password: string;
+
+  @Column()
+  role: string; // president,secretary,treasurer,member
+
+  @Column()
+  createdBy: string; // presidentId who created the user
 }
