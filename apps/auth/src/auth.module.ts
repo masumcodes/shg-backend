ac3ffc9c5ea6d22dd4114c664ab45bb070b4f8d9
Sneha Pal <Sneha1289pal@gmail.com>
2025-07-02 22:11:19 +0530

    Making changes to the auth module and users module in the NestJS application.

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,26 +1,29 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { AuthController } from './auth.controller';
-// import { AuthService } from './auth.service';
 import { User } from './entities/user.entity';
 import { UsersModule } from './users/users.module';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { ConfigModule } from '@nestjs/config';
+import { ClientsModule } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'sneha',
-      database: 'new',
-      entities: [User],
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
       synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }),
-    TypeOrmModule.forFeature([User]),
-    UsersModule,
+    UsersModule.register(),
   ],
   controllers: [UsersController],
   providers: [UsersService],
--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -1,12 +1,42 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-@Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UsersController],
-  providers: [UsersService],
-})
-export class UsersModule {}
+@Module({})
+export class UsersModule {
+  static register(): DynamicModule {
+    return {
+      module: UsersModule,
+      imports: [
+        ConfigModule,
+        TypeOrmModule.forFeature([User]),
+        ClientsModule.registerAsync([
+          {
+            name: 'USER_SERVICE',
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => {
+              const url = config.get<string>('USER_SERVICE_URL')!;
+              const pkg = config.get<string>('USER_SERVICE_PACKAGE')!;
+              const protoPath = config.get<string>('USER_SERVICE_PROTO_PATH')!;
+              return {
+                transport: Transport.GRPC,
+                options: {
+                  url,
+                  package: pkg,
+                  protoPath,
+                },
+              };
+            },
+          },
+        ]),
+      ],
+      controllers: [UsersController],
+      providers: [UsersService],
+      exports: [UsersService],
+    };
+  }
+}
